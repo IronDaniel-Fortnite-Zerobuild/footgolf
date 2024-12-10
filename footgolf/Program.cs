b270841c9854f925fa27e4225e74d08047bdb026
IronDániel-Fortnite-ZeroBuild <163414833+IronDaniel-Fortnite-Zerobuild@users.noreply.github.com>
2024-12-10 12:45:16 +0100

    Add project files.

---
+++ footgolf/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace footgolf
+{
+    internal class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            List<verseny> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("fob2016.txt"));
+            {
+                versenyzok.Add(new verseny(sor));
+            }
+            Console.WriteLine($"3.feladat enyi versenyzö indult el mindketö kategóriába: {versenyzok.Count()} !");
+
+            int 
+
+
+
+
+
+
+        }
+    }
+}
---
+++ footgolf/footgolf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verseny
+{
+    internal class verseny
+    {
+        public string Nev { get; set; }
+
+        public string Nem { get; set; }
+
+        public string csapat { get; set; }
+
+        public string kategoria { get; set; }
+
+        public string egyesület { get; set; }
+
+        public verseny(string sor)
+        {
+            string[] sprite = sor.Split(';');
+
+            Nev = sprite[0];
+            Nem = sprite[1];
+            csapat = sprite[2];
+            kategoria = sprite[3];
+            egyesület = sprite[4];
+        }
+    }
+}
